1cc84b0d344b6c6510d9c4d926b0415729b5b12e
Hanna Lam <hannalam.momo@gmail.com>
2024-01-29 18:01:23 +0100

    upload

---
+++ README.txt
@@ -0,0 +1,37 @@
+https://hub.labs.coursera.org:443/connect/sharedhtgyrcto?forceRefresh=false&path=
+
+Homepage
+●	header
+    ○	logo
+    ○   Shop 
+    ○   Login 
+    ○	top navigation
+        ■	Homepage
+        ■	Yoga Poses
+        ■	Online Class
+        ■	Healthy Diet
+        ■	Shop
+    ○   Search bar
+    ○   Social media bar
+    ○   Contact bar
+●	main
+    ○	Notice bar
+    ○	slider
+    ○	updates
+    ○	content blocks
+●	footer
+    ○	Wireframe
+    ○	Report 
+    ○	Sitemap
+
+Yoga Poses
+●	list of yoga poses, with photos and descriptions
+
+Online Class
+●	list of yoga classes, with photos and video 
+
+Healthy Diet
+●	list of meun, with videos and descriptions
+
+Shop
+●	list of yoga items and reviews, with photos and descriptions
---
+++ js/YogaposeData.js
@@ -0,0 +1,66 @@
+//this file contains the data for yogaposes.html
+//all the photos are original photos from me
+//all the description about yoga are from my teaching experience and from Hong Kong Yoga Association
+
+var yogaposes_data = {"yogaposes":[
+    {
+        pose :"img/yogaposes/yogapose1.jpg",
+        name : "Forward Bend",
+        description : "A complete stretch to the whole posterior of the body, from hip, spine, lower back, to thighs and calf. Increases flexibility of the spine. Strengthens and stretches the hamstrings."
+    },
+    {
+        pose :"img/yogaposes/yogapose2.jpg",
+        name : "Butterfly Pose",
+        description : "Improves flexibility of inner thigh joints. Activates thigh lymph regions. Stretching effect on lower back, inner thighs and hip. Relieves lower back pain."
+    },
+    {
+        pose :"img/yogaposes/yogapose3.jpg",
+        name : "Cobra Pose",
+        description: "Strengthens kidney, liver and pancreas. Prevents swelling of prostatic gland. Regulates thymus gland, thyroid gland and lymph regions of the neck. Increases sexual power."
+    },
+    {
+        pose :"img/yogaposes/yogapose4.jpg",
+        name : "Swan Pose",
+        description : "Releases tension from the shoulder region. Eases back problems and back pain. Combats menstrual irregularities and pain. Corrects rounded shoulders. Relieves abdominal ailments."
+    },
+    {
+        pose :"img/yogaposes/yogapose5.jpg",
+        name : "Spinal Twist",
+        description : "Improves digestion and bring gastric release. Lateral stretch of spine and back muscles. Powerfully massages all the abdominal organs. Stimulates the digestive organs and relieves constipation."
+    },
+    {
+        pose :"img/yogaposes/yogapose6.jpg",
+        name : "Plough Pose",
+        description : "All regions of spine and back muscles are stretched, the upper back and neck area in particular. Stimulates throat region. Brings blood to the brain, stimulates pituitary gland and has an anti-aging effect."
+    },
+    {
+        pose :"img/yogaposes/yogapose7.jpg",
+        name : "Fish Pose",
+        description : "Regulates thyroid gland secretions. Improves neck, shoulder and upper back pain. Stimulates thymus gland. Correct cervical vertebra"
+    },
+    {
+        pose :"img/yogaposes/yogapose8.jpg",
+        name : "Bow Pose",
+        description : "Fully bends and stretched spine. Activates the energy flow between the 7 Chakras. Strengthens kidney, liver and pancreas funtions."
+    },
+    {
+        pose :"img/yogaposes/yogapose9.jpg",
+        name : "Upward Plank",
+        description : "Strengthens lower back. Strengthens kidney. Improves sexual power. Strengthens triceps."
+    },
+    {
+        pose :"img/yogaposes/yogapose10.jpg",
+        name : "Side Plank",
+        description : "Strengthens the wrists, arms and stomach muscles. Develops concentration, balance and muscular coordination. Tones upper arm and wrist line."
+    },
+    {
+        pose :"img/yogaposes/yogapose11.jpg",
+        name : "Sleeping Buddha",
+        description : "Addresses insomnia and sleeping problems. Removes tension of shoulders. Strengthens the kidney. Backward bend to the spine improves flexibility of the lower back."
+    },
+    {
+        pose :"img/yogaposes/yogapose12.jpg",
+        name : "Triangle Pose",
+        description : "Stretches the armpit area, waist muscle and spine, stimulating the lymph system. Improves flexibility of the hip, spine and legs. Reduces lower back pain. Tones waist line."
+    }
+]};
